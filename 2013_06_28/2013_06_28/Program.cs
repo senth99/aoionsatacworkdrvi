1ab6a6ebbaa589b1e68339922b41d2eaf5946722
Senthuran Sakthikumar <senthurans@microtrends.net>
2013-07-02 09:42:57 -0400

    Added .net console project template

---
+++ 2013_06_28/2013_06_28/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2013_06_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
