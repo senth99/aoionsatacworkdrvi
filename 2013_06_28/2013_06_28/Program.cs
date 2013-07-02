e5c1a2da4a4d44944cfcae2a49013f330f70d39d
Senthuran Sakthikumar <senthurans@microtrends.net>
2013-07-02 10:37:26 -0400

    My first attempt - although I wish to try a solution using complex numbers

--- 2013_06_28/2013_06_28/Program.cs
+++ 2013_06_28/2013_06_28/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace _2013_06_28
 {
@@ -9,6 +6,27 @@ namespace _2013_06_28
     {
         static void Main(string[] args)
         {
+            var result = f(f(15));
+            Console.WriteLine(String.Format("{0}", result));
+            Console.ReadLine();
+        }
+
+        static int f(int n)
+        {
+            if (n > 0)
+            {
+                if (n % 2 == 0)
+                    return -1 * n + 1;
+                else
+                    return n + 1;
+            }
+            else
+            {
+                if (n % 2 == 0)
+                    return -1 * n - 1;
+                else
+                    return n - 1;
+            }
         }
     }
 }
